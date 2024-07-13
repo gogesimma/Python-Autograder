7a351756a69063f358ea7deb429fe758ce721251
Simakahle Goge <gogesimmah@gmail.com>
2024-07-13 15:36:50 +0200

    Integrate frontend with backend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UploadForm from './components/UploadForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Python Auto-Grader</h1>
+        <UploadForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
